709d5f037be36e4d0ce1581ef7c875b4d4bf64e6
Ryan McNair <ryan.mcnair@gmail.com>
2021-04-15 16:16:16 -0500

    full CRUD on Product Types

---
+++ DiaperJungle/Controllers/ProductTypesController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DiaperJungle.DataAccess;
+using DiaperJungle.Models;
+
+namespace DiaperJungle.Controllers
+{
+    [Route("api/ProductTypes")]
+    [ApiController]
+    public class ProductTypesController : ControllerBase
+    {
+        ProductTypeRepository _repo;
+
+        public ProductTypesController()
+        {
+            _repo = new ProductTypeRepository();
+        }
+        //get all
+        [HttpGet]
+        public IActionResult GetAllProductTypes()
+        {
+            return Ok(_repo.GetAll());
+        }
+        //get single
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var productType = _repo.Get(id);
+
+            if (productType == null)
+            {
+                return NotFound("This category does not exist");
+            }
+
+            return Ok(productType);
+        }
+
+        //post
+        [HttpPost]
+        public IActionResult AddAProductType(ProductType productType)
+        {
+            _repo.Add(productType);
+            return Created($"api/ProductTypes/{productType.id}", productType);
+        }
+
+        //delete
+        [HttpDelete("{productTypeId}")]
+        public IActionResult DeleteProductType(int productTypeId)
+        {
+            _repo.Remove(productTypeId);
+
+            return Ok();
+        }
+
+        //update
+        [HttpPut("{id}/update")]
+        public IActionResult UpdateProductType(int id, ProductType newCategory)
+        {
+            var productType = _repo.Get(id);
+
+            productType.category = newCategory.category;
+
+            _repo.Update(productType);
+
+            return NoContent();
+        }
+    }
+}
--- DiaperJungle/DataAccess/ProductTypeRepository.cs
+++ DiaperJungle/DataAccess/ProductTypeRepository.cs
@@ -2,10 +2,77 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DiaperJungle.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
 
 namespace DiaperJungle.DataAccess
 {
     public class ProductTypeRepository
     {
+        const string ConnectionString = "Server=localhost;Database=DiaperJungle;Trusted_Connection=True;";
+
+        //Get all product_types
+        public List<ProductType> GetAll()
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"Select *
+                        From Product_Type";
+
+            return db.Query<ProductType>(sql).ToList();
+        }
+
+        //Get single product_type
+        public ProductType Get(int id)
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"Select *
+                        From Product_Type
+                        where Id = @id";
+
+            var prodType = db.QueryFirstOrDefault<ProductType>(sql, new { id = id });
+
+            return prodType;
+        }
+
+        //Add product_type
+        public void Add(ProductType productType)
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"Insert into [Product_Type]([category])
+			            Output inserted.id
+			            Values(@category)";
+
+            var id = db.ExecuteScalar<int>(sql, productType);
+
+            productType.id = id;
+        }
+
+        //Delete product_type
+        public void Remove(int id)
+        {
+            var sql = @"Delete 
+                        from Product_Type 
+                        where Id = @id";
+
+            using var db = new SqlConnection(ConnectionString);
+
+            db.Execute(sql, new { id });
+        }
+
+        //Update product_type
+        public void Update(ProductType productType)
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"Update Product_Type
+                        Set category = @category
+                        where id = @id";
+
+            db.Execute(sql, productType);
+        }
     }
 }
