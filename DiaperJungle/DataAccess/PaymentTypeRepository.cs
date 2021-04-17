a7102a973dc4877b06ad3f4b79a466574e2cf9b6
josephtmartin <goldglovejo13@gmail.com>
2021-04-17 11:44:39 -0500

    still working on add payment type

--- DiaperJungle/DataAccess/PaymentTypeRepository.cs
+++ DiaperJungle/DataAccess/PaymentTypeRepository.cs
@@ -35,5 +35,25 @@ namespace DiaperJungle.DataAccess
 
             return singlePayType;
         }
+
+        public void Add(PaymentType paymentType, User user)
+        {
+            var sql = @"DECLARE @paymentTypeId TABLE (id int)
+
+                        INSERT INTO [dbo].[Payment_Type] ([pay_type], [account_number])
+                        OUTPUT inserted.id INTO @paymentTypeId
+                        VALUES(@pay_type, @account_number)
+
+                        DECLARE @id int = (SELECT TOP 1 id FROM @paymentTypeId)
+
+                        INSERT INTO [dbo].[User_Payment_Type] ([user_id], [payment_type_id])
+                        VALUES (@user_id, @id)";
+
+            using var db = new SqlConnection(ConnectionString);
+
+            var id = db.ExecuteScalar<int>(sql, paymentType);
+
+            paymentType.id = id;
+        }
     }
 }
