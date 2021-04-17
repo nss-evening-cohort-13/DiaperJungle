ae21268d401eb1de491b74f7cef6647415264999
Ryan McNair <ryan.mcnair@gmail.com>
2021-04-17 11:01:34 -0500

    Read & Delete on User_PaymentType join table

---
+++ DiaperJungle/Controllers/UsersPaymentTypesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DiaperJungle.Models;
+using DiaperJungle.DataAccess;
+
+namespace DiaperJungle.Controllers
+{
+    [Route("api/userspaymenttypes")]
+    [ApiController]
+    public class UsersPaymentTypesController : ControllerBase
+    {
+        UserPaymentTypeRepository _repo;
+
+        public UsersPaymentTypesController()
+        {
+            _repo = new UserPaymentTypeRepository();
+        }
+
+        //GET to /api/orders
+        [HttpGet]
+        public IActionResult GetAllUserPaymentTypes()
+        {
+            return Ok(_repo.GetAll());
+        }
+
+        //GET to /api/orders/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var order = _repo.Get(id);
+
+            if (order == null)
+            {
+                return NotFound("Not found");
+            }
+
+            return Ok(order);
+        }
+
+        [HttpDelete("{Id}")]
+        public IActionResult DeleteUserPaymentType(int id)
+        {
+            _repo.Remove(id);
+
+            return Ok();
+        }
+    }
+}
