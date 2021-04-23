4c46a401a03814e1245e6432a5616500414c36b8
Ryan McNair <ryan.mcnair@gmail.com>
2021-04-22 19:20:44 -0500

    GET all product types is working properly

--- diaperjungle.ui/src/helpers/routes.js
+++ diaperjungle.ui/src/helpers/routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Products from '../views/products';
+import ProductTypes from '../views/productTypes';
 
 export default function Routes() {
   return (
         <Switch>
-            <Route exact path="/products" component={Products} />
+            <Route exact path='/products' component={Products} />
+            <Route exact path='/producttypes' component={ProductTypes}/>
         </Switch>
   );
 }
