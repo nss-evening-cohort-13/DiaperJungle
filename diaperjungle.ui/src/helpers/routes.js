fb392db4f1b9c758d44545d34e2a1a98ba194d92
josephtmartin <goldglovejo13@gmail.com>
2021-04-22 19:14:00 -0500

    got the user data from the database and displayed it on the DOM

--- diaperjungle.ui/src/helpers/routes.js
+++ diaperjungle.ui/src/helpers/routes.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+
 import Products from '../views/products';
+import Users from '../views/users';
 
 export default function Routes() {
   return (
         <Switch>
             <Route exact path="/products" component={Products} />
+            <Route exact path="/users" component={Users} />
         </Switch>
   );
 }
