7ce78b7ba2a4348a3fe486271ba104cd700ec253
joe <goldglovejo13@gmail.com>
2021-04-25 14:19:07 -0500

    aded the search functionality and new dB data

--- diaperjungle.ui/src/views/searchResults.js
+++ diaperjungle.ui/src/views/searchResults.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import ProductCard from '../components/productCard';
+import { Link } from 'react-router-dom';
+// import ProductCard from '../components/productCard';
 import productData from '../helpers/data/productData';
 
 class SearchResults extends Component {
@@ -32,11 +33,24 @@ class SearchResults extends Component {
   render() {
     const { results } = this.state;
 
-    const showResults = () => results.map((result) => <ProductCard key={result.id} product={result} />);
+    // const showResults = () => results.map((result) => <ProductCard key={result.id} product={result} />);
+    const productCard = (product) => (
+      <div className='product-card'>
+      <div className='card m-2'>
+        <h5 className='card-title'>{product.title}</h5>
+        <div className='card-body'>
+        <p className='card-text'>{product.description}</p>
+        </div>
+        <Link className='btn btn-primary' to={`/products/${product.id}`}>Product Details</Link>{' '}
+      </div>
+      </div>);
+
+    const cards = results.map(productCard);
+
     return (
       <div>
         <h1>Search Results</h1>
-        <div className='product-cards-container'>{showResults()}</div>
+        <div className='product-cards-container'>{cards}</div>
       </div>
     );
   }
