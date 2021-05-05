a0cdbf6985714832da87783fffff58cc49a750d7
Steven Zelenak <stevenmzelenak@gmail.com>
2021-05-04 22:00:03 -0500

    progress :D

--- diaperjungle.ui/src/views/products.js
+++ diaperjungle.ui/src/views/products.js
@@ -8,8 +8,9 @@ import AnimalTypeFilter from '../components/animalTypeFilter';
 class Products extends React.Component {
   state = {
     products: [],
-    isSelected: [],
+    isSelectedCheckbox: [],
     isSelectedDropdown: [],
+    isSelectedFiltered: [],
     animalId: -1,
   };
 
@@ -47,40 +48,40 @@ class Products extends React.Component {
       );
       this.setState({
         isSelectedDropdown: animalMatchArray,
-      });
+      }, this.filterCheckboxAndDropdown);
       // If there is already an animal picked clear state get the new id re-filter and add back to state the new list
     } else if (animalTypeId !== -1 && isSelectedDropdown.length > 0) {
       this.setState({
         isSelectedDropdown: [],
-      });
+      }, this.filterCheckboxAndDropdown);
       animalTypeId = parseInt(this.state.animalId, 10);
       const animalMatchArray = products.filter(
         (product) => product.animal_type_id === animalTypeId
       );
       this.setState({
         isSelectedDropdown: animalMatchArray,
-      });
+      }, this.filterCheckboxAndDropdown);
     }
   }
 
   filterProducts = (e) => {
     // getting the typed id from filter? storing it in checkedtype
     const checkedType = parseInt(e.target.value, 10);
-    const { isSelected, products } = this.state;
-    // if the event target is checked and the length of isSelected is 0 filter over products find the type_id that is = to checked type and
-    // set isSelected in state to the information passed.
-    if (e.target.checked && isSelected.length === 0) {
+    const { isSelectedCheckbox, products } = this.state;
+    // if the event target is checked and the length of isSelectedCheckbox is 0 filter over products find the type_id that is = to checked type and
+    // set isSelectedCheckbox in state to the information passed.
+    if (e.target.checked && isSelectedCheckbox.length === 0) {
       const newChecked = products.filter(
         (product) => product.type_id === checkedType
       );
       this.setState({
-        isSelected: newChecked,
-      });
-      // if the event target is checked and the length of isSelected is > then 0 currently checked is = to isSelected
+        isSelectedCheckbox: newChecked,
+      }, this.filterCheckboxAndDropdown);
+      // if the event target is checked and the length of isSelectedCheckbox is > then 0 currently checked is = to isSelectedCheckbox
       // then we create a new variable where we filter over products again then forEach over currently checked for each product in currently checked we push to newChecked
-      // set state isSelected to newChecked
-    } else if (e.target.checked && isSelected.length > 0) {
-      const currentlyChecked = isSelected;
+      // set state isSelectedCheckbox to newChecked
+    } else if (e.target.checked && isSelectedCheckbox.length > 0) {
+      const currentlyChecked = isSelectedCheckbox;
       const newChecked = products.filter(
         (product) => product.type_id === checkedType
       );
@@ -88,23 +89,42 @@ class Products extends React.Component {
         newChecked.push(product);
       });
       this.setState({
-        isSelected: newChecked,
-      });
+        isSelectedCheckbox: newChecked,
+      }, this.filterCheckboxAndDropdown);
       // if the event target is not checked
     } else if (!e.target.checked) {
-      const noMatch = isSelected.filter(
+      const noMatch = isSelectedCheckbox.filter(
         (product) => product.type_id !== checkedType
       );
       this.setState({
-        isSelected: noMatch,
-      });
+        isSelectedCheckbox: noMatch,
+      }, this.filterCheckboxAndDropdown);
     }
   };
 
+  //only show stuff that is matching need to set conditions for when things don't match or for when only the checkboxes or the dropdown is used
+  filterCheckboxAndDropdown = () => {
+    const thingArray = [];
+    // loop over isSelectedCheck
+    this.state.isSelectedCheckbox.forEach((animalCheck) => {
+      // loop over isSelectedDrop
+      this.state.isSelectedDropdown.forEach((animalDrop) => {
+        // find the products that match both conditions
+        if (animalCheck.animal_type_id === animalDrop.id) {
+          thingArray.push(animalDrop);
+        }
+      });
+    });
+    // set the state of isSelectedFilter with the results
+    this.setState({
+      isSelectedFiltered: thingArray,
+    });
+  }
+
   render() {
-    const { isSelected, products } = this.state;
+    const { isSelectedFiltered, products } = this.state;
     const renderAllProductCards = () => products.map((product) => (<ProductCard key={product.id} product={product} />));
-    const renderSelectedCards = () => isSelected.map((product) => (<ProductCard key={product.id} product={product} />));
+    const renderSelectedCards = () => isSelectedFiltered.map((product) => (<ProductCard key={product.id} product={product} />));
     return (
       <>
         <h2>Products</h2>
@@ -118,7 +138,7 @@ class Products extends React.Component {
           Add Products
         </Link>
         <div>
-          {isSelected.length === 0
+          {isSelectedFiltered.length === 0
             ? renderAllProductCards()
             : renderSelectedCards()}
         </div>
