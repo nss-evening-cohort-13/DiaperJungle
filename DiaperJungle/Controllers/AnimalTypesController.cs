fd6a7f72e78786366ce55e6eae079722c0eac1fe
Steven Zelenak <stevenmzelenak@gmail.com>
2021-05-06 20:51:49 -0500

    fixed

--- DiaperJungle/Controllers/AnimalTypesController.cs
+++ DiaperJungle/Controllers/AnimalTypesController.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using DiaperJungle.DataAccess;
 using DiaperJungle.Models;
-
 namespace DiaperJungle.Controllers
 {
     [Route("api/AnimalTypes")]
@@ -14,33 +13,27 @@ namespace DiaperJungle.Controllers
     public class AnimalTypesController : ControllerBase
     {
         AnimalTypeRepository _repo;
-
         public AnimalTypesController()
         {
             _repo = new AnimalTypeRepository();
         }
-
         //GET to /api/AnimalTypes
         [HttpGet]
         public IActionResult GetAllProductTypes()
         {
             return Ok(_repo.GetAll());
         }
-
         //GET to /api/AnimalTypes/{id}
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
             var productType = _repo.Get(id);
-
             if (productType == null)
             {
                 return NotFound("This category does not exist");
             }
-
             return Ok(productType);
         }
-
         //POST to /api/AnimalTypes
         [HttpPost]
         public IActionResult AddAProductType(AnimalsType animalType)
@@ -48,25 +41,20 @@ namespace DiaperJungle.Controllers
             _repo.Add(animalType);
             return Created($"api/AnimalTypes/{animalType.id}", animalType);
         }
-
         //DELETE /api/AnimalTypes/{AnimalTypesId}
         [HttpDelete("{animalTypeId}")]
         public IActionResult DeleteProductType(int animalTypeId)
         {
             _repo.Remove(animalTypeId);
-
             return Ok();
         }
-
         //PUT to /api/{AnimalTypesId}/update
         [HttpPut("{id}/update")]
+        public IActionResult UpdateProductType(int id, AnimalsType newCategory)
         {
             var animalType = _repo.Get(id);
-
             animalType.animal_category = newCategory.animal_category;
-
             _repo.Update(animalType);
-
             return NoContent();
         }
     }
