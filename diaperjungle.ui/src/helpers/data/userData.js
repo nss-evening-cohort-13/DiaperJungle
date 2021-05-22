2a0ce7da1998a7ae05055df962dcc6ed8e22ce3b
Steven Zelenak <stevenmzelenak@gmail.com>
2021-05-22 10:29:06 -0500

    search bar done

--- diaperjungle.ui/src/helpers/data/userData.js
+++ diaperjungle.ui/src/helpers/data/userData.js
@@ -15,4 +15,10 @@ const getUserByFBUid = (fbUid) => new Promise((resolve, reject) => {
   }).catch((error) => reject(error));
 });
 
-export default { getAllUsers, getUserByFBUid };
+const getSearchedUsers = (searchTerm) => new Promise((resolve, reject) => axios
+  .get(`${userUrl}`).then((response) => {
+    const searched = response.data.filter((user) => user.first_name.toLowerCase().includes(searchTerm));
+    resolve(searched);
+  }).catch((error) => reject(error)));
+
+export default { getAllUsers, getUserByFBUid, getSearchedUsers };
--- diaperjungle.ui/src/views/admin.js
+++ diaperjungle.ui/src/views/admin.js
@@ -2,14 +2,50 @@ import React from 'react';
 import { CardGroup } from 'react-bootstrap';
 import orderData from '../helpers/data/orderData';
 import OrderCard from '../components/orderCard';
+import userData from '../helpers/data/userData';
+import UserCard from '../components/userCard';
 
 export default class Admin extends React.Component {
   state = {
     orders: [],
+    results: [],
+    text: '',
+    userSearched: false,
   };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      userSearched: true,
+    });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  populateResults = () => {
+    const searchTerm = this.state.text;
+
+    userData.getSearchedUsers(searchTerm.toLowerCase()).then((response) => {
+      this.setState({
+        results: response,
+        searchTerm,
+      });
+    });
+  };
+
+  componentDidUpdate(prevState) {
+    if (prevState.searchTerm !== this.state.text) {
+      this.populateResults();
+    }
+  }
 
   componentDidMount() {
     this.getAllTheOrders();
+    this.populateResults();
   }
 
   getAllTheOrders = () => {
@@ -21,9 +57,9 @@ export default class Admin extends React.Component {
   }
 
   render() {
-    const { orders } = this.state;
+    const { orders, results } = this.state;
     const renderAllOrderCards = () => orders.map((order) => <OrderCard key={order.id} order={order}/>);
-
+    const renderUsersCards = () => results.map((user) => <UserCard key={user.id} user={user}/>);
     let orderTotal;
     if (orders.length !== 0) {
       let total = 0;
@@ -36,10 +72,26 @@ export default class Admin extends React.Component {
     return (
       <>
         <h2>Admin</h2>
+        <form onSubmit={this.handleSubmit}>
+        <label className='p-1'>
+          Search:
+            <input type='text' name='text' value={this.state.text} onChange={this.handleChange} />
+          </label>
+        </form>
         <h2>Total Of All Orders: ${`${orderTotal}`}</h2>
         <CardGroup className='order-cards-container'>
           {renderAllOrderCards()}
         </CardGroup>
+        <div>
+          {this.state.userSearched ? (
+            <div>
+              <h1>Users Searched</h1>
+              {renderUsersCards()}
+            </div>
+          ) : (
+            <p></p>
+          )}
+        </div>
       </>
     );
   }
